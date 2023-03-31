48738df97f98d4b372dfa61c07aca0f139baa0e9
Asim-hameed <asimhameed31@gmail.com>
2023-03-31 19:28:35 +0500

    Sealed Methods added

--- Sealed_C#/Program.cs
+++ Sealed_C#/Program.cs
@@ -1,14 +1,36 @@
 ﻿namespace Sealed_C_
 {
+    //Sealed Classes cannot be inherited or it cannnot make a child 
     class A
     {
+        public virtual void print()
+        {
+            Console.WriteLine("Method of class A");
+        }
+    }
 
+    //Sealed method cannot be further overriden by any other class method of same name
+    class B : A 
+    {
+        public sealed override void print()
+        {
+            Console.WriteLine("Method of class B");
+        }
+    }
+    class C : B
+    {
+        public override void print()
+        {
+            Console.WriteLine("Method of class C");
+        }
     }
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            C obj = new C();
+            obj.print();
         }
     }
 }
